905f97a6eee76d45a65b55dca0023d85689c6405
Kevin George <kevingeorgetm@gmail.com>
2016-04-02 02:17:00 +0000

    Added docs

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,29 @@
-/*
-   This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-   It won't work with v1.x motor shields! Only for the v2's with built in PWM
-   control
-
-   For use with the Adafruit Motor Shield v2
-   ---->	http://www.adafruit.com/products/1438
- */
-
+/*UT IEEE RAS Mercury 2016*/
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
-// Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
-
-// Select which 'port' M1, M2, M3 or M4. In this case, M1
 Adafruit_DCMotor *myMotor = AFMS.getMotor(3);
-// You can also make another motor on port M2
-//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
 
-
-/*Motor variables*/
+/*Motor Variables*/
 int left_motor_speed;
 int right_motor_speed;
 /*Tracks first byte and byte before that*/
 char last_read_byte = 0;
 char current_byte = 0;
+/*Motor Variables*/
 
 enum MOTOR_DIRECTION {forward, backward};
 int motor_direction;
 void updateMotors();
 
-void setup() { /**/
-        Serial.begin(9600);     // set up Serial library at 9600 bps
+void setup() { 
+        Serial.begin(9600); 
         Serial.println("Adafruit Motorshield v2 - DC Motor test!");
-
-        AFMS.begin(); // create with the default frequency 1.6KHz
-        //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
-
-        // Set the speed to start, from 0 (off) to 255 (max speed)
+        AFMS.begin(); 
         myMotor->setSpeed(150);
         myMotor->run(FORWARD);
-        // turn on motor
         myMotor->run(RELEASE);
 }
 
